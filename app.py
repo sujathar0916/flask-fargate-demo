2c465eefde0faade6f269e017632e7f8c22ccec4
sujathar0916 <sujathar0916@gmail.com>
2025-11-26 22:46:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from AWS Fargate on ECS via ALB!"
+    return "Hello from Github CI/CD pipeline"
 
 if __name__ == "__main__":
     # Bind to 0.0.0.0 so container port is accessible
    app.run(host="0.0.0.0", port=80)
+
